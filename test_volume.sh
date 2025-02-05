93ba71fdb44df195882d786577f479a4755000e9
yashshah0401 <yshah0493@conestogac.on.ca>
2025-02-05 15:01:16 -0500

    Add custom test cases for volume calculation

--- test_volume.sh
+++ test_volume.sh
@@ -17,10 +17,12 @@ test_volume() {
 
 # Test cases
 test_volume 2 3 4 24
-test_volume 1 1 1 1
-
+test_volume 5 5 5 125
 # Failing test cases
 test_volume 0 3 4 "Invalid input"
 
-# Additional failing test cases
-test_volume abc 3 4 "Invalid input"
+# Additional custom test cases
+test_volume 3 4 5 60
+test_volume -2 3 4 "Invalid input"
+test_volume 2 3 abc "Invalid input"
+
