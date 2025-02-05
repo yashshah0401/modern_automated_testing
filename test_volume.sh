d8c8c0a40ab1e604fe76c1f9abc7c031f84f6220
yashshah0401 <yshah0493@conestogac.on.ca>
2025-02-05 15:10:38 -0500

    Added the alphabetical value

--- test_volume.sh
+++ test_volume.sh
@@ -11,18 +11,25 @@ test_volume() {
   if [ "$actual_output" == "$expected_output" ]; then
     echo "Test passed!"
   else
-    echo "Test failed! Expected '$expected_output' but got '$actual_output'"
+    echo "Test failed! Input: height=$height, width=$width, length=$length. Expected '$expected_output' but got '$actual_output'"
   fi
 }
 
 # Test cases
 test_volume 2 3 4 24
+test_volume 1 1 1 1
 test_volume 5 5 5 125
+
 # Failing test cases
 test_volume 0 3 4 "Invalid input"
+test_volume -1 3 4 "Invalid input"
 
 # Additional custom test cases
 test_volume 3 4 5 60
+test_volume 0 2 3 "Invalid input"
 test_volume -2 3 4 "Invalid input"
 test_volume 2 3 abc "Invalid input"
 
+# Intentionally failing test cases
+test_volume 2 2 2 10  # This should fail
+test_volume 3 3 3 30  # This should fail
