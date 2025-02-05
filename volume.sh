460ea2b3c3bb1e751a8acf700e2f0ba0426f511a
yashshah0401 <yshah0493@conestogac.on.ca>
2025-02-05 14:54:11 -0500

    Handle invalid inputs in volume calculation script

--- volume.sh
+++ volume.sh
@@ -4,5 +4,11 @@ height=$1
 width=$2
 length=$3
 
+# Check for invalid inputs
+if [ "$height" -le 0 ] || [ "$width" -le 0 ] || [ "$length" -le 0 ]; then
+  echo "Invalid input"
+  exit 1
+fi
+
 volume=$((height * width * length))
 echo $volume
