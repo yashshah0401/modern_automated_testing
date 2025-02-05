8737759778b8590a210fd1129dddd096820daa9d
yashshah0401 <yshah0493@conestogac.on.ca>
2025-02-05 14:48:42 -0500

    Add more test cases for volume calculation

--- test_volume.sh
+++ test_volume.sh
@@ -2,10 +2,10 @@
 
 # Test case for volume calculation
 test_volume() {
-  local height=2
-  local width=3
-  local length=4
-  local expected_volume=24
+  local height=$1
+  local width=$2
+  local length=$3
+  local expected_volume=$4
   local actual_volume=$(./volume.sh $height $width $length)
   
   if [ "$actual_volume" -eq "$expected_volume" ]; then
@@ -15,4 +15,7 @@ test_volume() {
   fi
 }
 
-test_volume
+# Test cases
+test_volume 2 3 4 24
+test_volume 1 1 1 1
+test_volume 5 5 5 125
